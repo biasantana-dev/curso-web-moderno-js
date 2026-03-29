6b65f97725e39bb1e9fe900d6846afbc1c09cbd3
Bia Santana <beatrizsantana0914@gmail.com>
2026-03-29 20:55:57 -0300

    Create tipagemFraca.js

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+
+qualquer = 3.1545
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nome genérico e siglas
+let valor = '';
+let numero = 1;
+let pqp = false // Produto Quimico Perigoso...
