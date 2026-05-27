bbb0c841c52a800135a39c69f619b9d4dddc02c9
Bia Santana <beatrizsantana0914@gmail.com>
2026-05-27 20:12:07 -0300

    Adicionar funções para escolher frutas e comprar carros

---
+++ Fundamentos/exercicios/ex14.js
@@ -0,0 +1,21 @@
+function escolherFruta(frutas) {
+   switch (frutas) {
+      case 'maça':
+         console.log('Não vendemos esta fruta aqui.');
+         break;
+      case 'kiwi':
+         console.log('Estamos com escassez de kiwis.');
+         break;
+      case 'melancia':
+         console.log('Aqui está, são 3 reais o quilo.');
+         break;
+      default:
+         console.log('Opção inválida! Tente novamente.');
+         break;
+   }
+}
+
+escolherFruta('maça');
+escolherFruta('kiwi');
+escolherFruta('melancia');
+escolherFruta('manga');
