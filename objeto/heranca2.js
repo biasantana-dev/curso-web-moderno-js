13c0dd851c5030e24ebe6d42dd8383559ee3258d
Bia Santana <beatrizsantana0914@gmail.com>
2026-06-05 20:22:56 -0300

    Adicionar exemplos de uso da cadeia de protótipos e métodos de aceleração em objetos JavaScript

--- objeto/heranca2.js
+++ objeto/heranca2.js
@@ -1,6 +1,6 @@
 // Cadeia de protótipos (prototype chain)
-Object.prototype.attr0 = '0';
-const avo = { attr1: 'A' };
+Object.prototype.attr0 = '0'; // não faça isso!!!
+const avo = { attr1: 'A' }; 
 const pai = { __proto__: avo, attr2: 'B', attr3: '3' };
 const filho = { __proto__: pai, attr3: 'C' };
 console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3);
@@ -33,3 +33,13 @@ const volvo = {
 };
 
 Object.setPrototypeOf(ferrari, carro);
+Object.setPrototypeOf(volvo, carro);
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.acelecarMais(100);
+console.log(volvo.status());
+
+ferrari.acelecarMais(300);
+console.log(ferrari.status());
