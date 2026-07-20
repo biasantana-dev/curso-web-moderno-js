61d65f5c6881aae57e3bc311f94d09868d978f40
Bia Santana <beatrizsantana0914@gmail.com>
2026-07-20 19:03:44 -0300

    Adicionar arquivos filter1.js e filter2.js com implementações de filtros personalizados para arrays

---
+++ array/filter1.js
@@ -0,0 +1,12 @@
+const produtos = [
+   { nome: 'Notebook', preco: 2499, fragil: true },
+   { nome: 'IPad Pro', preco: 4199, fragil: true },
+   { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+   { nome: 'Copo de Plastico', preco: 18.99, fragil: false }
+
+];
+
+const caro = produto => produto.preco >= 500;
+const fragil = produto => produto.fragil; // não precisa de comparação, pois já retorna true
+
+console.log(produtos.filter(caro).filter(fragil));
